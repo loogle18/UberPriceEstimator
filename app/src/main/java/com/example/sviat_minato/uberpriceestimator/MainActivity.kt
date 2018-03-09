91fe7a46ba19987d98c15db91fc8b9be40dc31ae
loogle18 <sme@jetthoughts.com>
2018-03-09 23:55:43 +0200

    Added improvements for MainAktivity. Added AlertDialog instead of Toast on click button. Added checking for google play services availability.

--- app/src/main/java/com/example/sviat_minato/uberpriceestimator/MainActivity.kt
+++ app/src/main/java/com/example/sviat_minato/uberpriceestimator/MainActivity.kt
@@ -4,10 +4,12 @@ import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.location.places.ui.PlaceAutocomplete
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import android.content.Intent
+import android.support.v7.app.AlertDialog
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,17 +20,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         editFrom = findViewById<EditText>(R.id.edit_from)
         editTo = findViewById<EditText>(R.id.edit_to)
         buttonGetPrice = findViewById<Button>(R.id.button_get_price)
 
-        val autocompleteIntent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
-                .build(this)
+        val playServicesConnectionResult = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        handlePlayServicesConnectionResult(playServicesConnectionResult)
 
-        editFromAndToClicked(autocompleteIntent)
-        buttonGetPriceClicked()
+        if (playServicesConnectionResult == ConnectionResult.SUCCESS) {
+            val autocompleteIntent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .build(this)
+
+            editFromAndToClicked(autocompleteIntent)
+            buttonGetPriceClicked()
+        }
     }
-
 
     private fun editFromAndToClicked(intent: Intent) {
         editFrom.setOnClickListener {
@@ -43,12 +50,35 @@ class MainActivity : AppCompatActivity() {
     private fun buttonGetPriceClicked() {
         buttonGetPrice.setOnClickListener() {
             if (editFrom.text.isNotBlank() && editTo.text.isNotBlank()) {
-                Toast.makeText(this, "Getting price for ride from ${editFrom.text} to ${editFrom.text}",
-                        Toast.LENGTH_SHORT).show()
+                showAlert("Getting price for ride from ${editFrom.text} to ${editFrom.text}")
             }
         }
     }
 
+    private fun showAlert(message: String) {
+        val alert = AlertDialog.Builder(this)
+
+        with (alert) {
+            setMessage(message)
+
+            setPositiveButton("Close") { dialog, whichButton ->
+                dialog.dismiss()
+            }
+        }
+
+        alert.show()
+    }
+
+    private fun handlePlayServicesConnectionResult(result: Int) {
+        when (result) {
+            ConnectionResult.SERVICE_MISSING ->
+                GoogleApiAvailability.getInstance().getErrorDialog(this, ConnectionResult.SERVICE_MISSING, 0).show()
+            ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED ->
+                GoogleApiAvailability.getInstance().getErrorDialog(this, ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED, 0).show()
+            ConnectionResult.SERVICE_DISABLED ->
+                GoogleApiAvailability.getInstance().getErrorDialog(this, ConnectionResult.SERVICE_DISABLED, 0).show()
+        }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         if (listOf(1, 2).contains(requestCode)) {
@@ -57,7 +87,7 @@ class MainActivity : AppCompatActivity() {
                 val lat = place.latLng.latitude
                 val lng = place.latLng.longitude
                 val address = place.address
-                var editText = if (requestCode == 1) this.editFrom else this.editTo
+                val editText = if (requestCode == 1) this.editFrom else this.editTo
 
                 editText.setText(address)
 
