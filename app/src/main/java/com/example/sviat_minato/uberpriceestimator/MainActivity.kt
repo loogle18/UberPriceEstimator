5b7239af3429886f6a05a331fe32a4f2aa360b0c
loogle18 <sme@jetthoughts.com>
2018-03-10 02:43:33 +0200

    Improved PlaceAutocomplete. Configured permission checks and permission requests for COARSE_LOCATION. Configured location manager to request location updates if permission is granted. Configured PlaceAutocomplete to use bounds bias when location is available.

--- app/src/main/java/com/example/sviat_minato/uberpriceestimator/MainActivity.kt
+++ app/src/main/java/com/example/sviat_minato/uberpriceestimator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sviat_minato.uberpriceestimator
 
+import android.Manifest
 import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,15 +8,31 @@ import com.google.android.gms.location.places.ui.PlaceAutocomplete
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.location.LocationProvider
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.widget.Button
 import android.widget.EditText
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.SphericalUtil
+import com.google.android.gms.maps.model.LatLng
+
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var autocompleteIntent: Intent
     lateinit var editFrom: EditText
     lateinit var editTo: EditText
     lateinit var buttonGetPrice: Button
+    private var locationManager: LocationManager? = null
+    var latLngBounds: LatLngBounds? = null
+    var isLocationAvailable = false
+    var MY_REQUEST_ACCESS_COARSE_LOCATION = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +41,14 @@ class MainActivity : AppCompatActivity() {
         editFrom = findViewById<EditText>(R.id.edit_from)
         editTo = findViewById<EditText>(R.id.edit_to)
         buttonGetPrice = findViewById<Button>(R.id.button_get_price)
-
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
         val playServicesConnectionResult = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
         handlePlayServicesConnectionResult(playServicesConnectionResult)
 
         if (playServicesConnectionResult == ConnectionResult.SUCCESS) {
-            val autocompleteIntent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
-                    .build(this)
-
+            requestNeededPermissions()
+            requestLocationUpdates()
+            initAutocompleteIntent()
             editFromAndToClicked(autocompleteIntent)
             buttonGetPriceClicked()
         }
@@ -77,6 +94,82 @@ class MainActivity : AppCompatActivity() {
                 GoogleApiAvailability.getInstance().getErrorDialog(this, ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED, 0).show()
             ConnectionResult.SERVICE_DISABLED ->
                 GoogleApiAvailability.getInstance().getErrorDialog(this, ConnectionResult.SERVICE_DISABLED, 0).show()
+        }
+    }
+
+    private fun initAutocompleteIntent() {
+        if (isLocationAvailable) {
+            latLngBounds?.let {
+                autocompleteIntent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                        .setBoundsBias(it)
+                        .build(this)
+            }
+        } else {
+            autocompleteIntent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .build(this)
+        }
+    }
+
+    private fun toBounds(center: LatLng): LatLngBounds {
+        val distanceFromCenterToCorner = 1000 * Math.sqrt(2.0)
+        val southwestCorner = SphericalUtil.computeOffset(center, distanceFromCenterToCorner, 225.0)
+        val northeastCorner = SphericalUtil.computeOffset(center, distanceFromCenterToCorner, 45.0)
+        return LatLngBounds(southwestCorner, northeastCorner)
+    }
+
+    private fun requestNeededPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    MY_REQUEST_ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_REQUEST_ACCESS_COARSE_LOCATION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    requestLocationUpdates()
+                    initAutocompleteIntent()
+                    editFromAndToClicked(autocompleteIntent)
+                }
+            }
+        }
+    }
+
+    private fun requestLocationUpdates() {
+        try {
+            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+            locationManager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)?.let {
+                latLngBounds = toBounds(LatLng(it.latitude, it.longitude))
+            }
+            isLocationAvailable = true
+        } catch(exception: SecurityException) {
+            println(exception)
+        }
+    }
+
+    private val locationListener: LocationListener = object: LocationListener {
+        override fun onLocationChanged(location: Location) {
+            latLngBounds = toBounds(LatLng(location.latitude, location.longitude))
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+            when (status) {
+                LocationProvider.OUT_OF_SERVICE -> isLocationAvailable = false
+                LocationProvider.TEMPORARILY_UNAVAILABLE -> isLocationAvailable = false
+                LocationProvider.AVAILABLE -> isLocationAvailable = true
+            }
+        }
+
+        override fun onProviderEnabled(provider: String) {
+            isLocationAvailable = true
+        }
+
+        override fun onProviderDisabled(provider: String) {
+            isLocationAvailable = false
         }
     }
 
