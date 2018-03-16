5a3800f8f70b7a3b959d1fafc7614834d9645cad
loogle18 <sviat.minato@gmail.com>
2018-03-16 17:16:12 +0200

    Added PendingIntent.FLAG_ONE_SHOT to prevent from multiple activity creation.

--- app/src/main/java/com/example/sviat_minato/uberpriceestimator/GetLowerPriceActivity.kt
+++ app/src/main/java/com/example/sviat_minato/uberpriceestimator/GetLowerPriceActivity.kt
@@ -111,7 +111,7 @@ class GetLowerPriceActivity : AppCompatActivity() {
         intent.putExtra("notificationTitle", title)
         intent.putExtra("notificationText", message)
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_ONE_SHOT)
         notificationBuilder.setContentIntent(pendingIntent)
 
         notificationManager.notify(1, notificationBuilder.build())
